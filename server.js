257964ec8df6ccc46d4a370a0118999070953c9d
Jeffery Whittington <Jefferious@gmail.com>
2023-08-17 13:48:51 -0600

    added rollbar

--- server.js
+++ server.js
@@ -2,5 +2,15 @@ const express = require('express')
 const app = express()
 
 app.use(express.static(`${__dirname}/public`))
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'f43eb033a5ee4de793ab28fe2c79e271',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 app.listen(4000, () => console.log("Server running on 4000"))
